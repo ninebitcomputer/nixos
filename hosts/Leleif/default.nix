d8e59cc75ecfa3705058226e372d3be4478ed7ed
Daniel Jin <mrpickles123115@gmail.com>
2025-11-29 16:00:02 -0500

    add host

---
+++ hosts/Leleif/default.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, inputs, ... }:
+
+{
+  imports = [
+      ../../modules/audio.nix
+      ../../modules/fonts.nix
+      ../../modules/system.nix
+      ../../modules/extraHosts.nix
+
+	  ../../modules/extra/podman.nix
+      ../../modules/extra/ssh.nix
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "Leleif"; # Define your hostname.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  time.timeZone = "America/Fort_Wayne";
+  system.stateVersion = "24.11";
+
+}
+
